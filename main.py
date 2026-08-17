f2eec6728ed139bec8576af39b7551cbcb416d21
brody-b9801 <brodymb2@illinois.edu>
2026-08-17 12:32:02 -0500

    schedule command written, creates the table if it doesnt exist and inserts the event information. needs logic to ensure eventname is unique within each server

--- main.py
+++ main.py
@@ -20,12 +20,30 @@ async def on_ready():
 
 @bot.command()
 async def schedule(ctx, name: str, month: int, day: int, year: int):
-    date = f"{month}/{day}/{year}"
+    date = f"{month}/{day}/{year}"    
+    guild_id = ctx.guild.id
+    cur.execute(f"""
+        CREATE TABLE IF NOT EXISTS events (
+            guild TEXT PRIMARY KEY NOT NULL UNIQUE,
+            eventname TEXT NOT NULL,
+            month INTEGER,
+            day INTEGER,
+            year INTEGER
+        )
+    """)
+
+    date_info = (guild_id, name, month, day, year)
+
+    cur.execute(f"INSERT INTO events (guild, eventname, month, day, year) VALUES (?, ?, ?, ?, ?)", date_info)
     await ctx.send(f"Event '{name}' created for {date}")
 
 @bot.command()
 async def countdown(ctx, name: str):
+    #retreive event from guild the command was sent from, compute days until the date using helper method
     await ctx.send(f"Countdown for event '{name}' is not implemented yet")
+
+async def delete(ctx, name: str):
+    await ctx.send("deleted")
 
 load_dotenv()
 bot.run(os.getenv("DISCORD_TOKEN"))
