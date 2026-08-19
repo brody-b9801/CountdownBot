f2d66d64c250968524e458eade44f8291763875d
Brody Blask <brodymb2@illinois.edu>
2026-08-18 23:02:31 -0500

    Refactor bot commands to use application commands

--- main.py
+++ main.py
@@ -9,22 +9,28 @@ description = """A discord bot to count down the days until an event"""
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='/', description=description, intents=intents)
 
 con = sqlite3.connect("event_database.db")
 cur = con.cursor()
+
+class Bot(commands.Bot):
+    async def setup_hook(self) -> None:
+        await self.tree.sync()
+
+bot = Bot(command_prefix='/', description=description, intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
 
-@bot.command()
-async def schedule(ctx, name: str, month: int, day: int, year: int):
+@bot.tree.command(name="schedule", description="schedule a countdown")
+async def schedule(ctx, name: str, month: int, day: int, year: int) -> None:
     sender = ctx.author
     guild_id = ctx.guild.id
 
     cur.execute(f"""
         CREATE TABLE IF NOT EXISTS events (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             guild TEXT PRIMARY KEY NOT NULL UNIQUE,
             sender TEXT NOT NULL,
             eventname TEXT NOT NULL,
@@ -34,7 +40,10 @@ async def schedule(ctx, name: str, month: int, day: int, year: int):
         )
     """)
 
-    cur.execute("SELECT * FROM events WHERE guild = ? AND sender = ? AND eventname = ?", (guild_id, sender, name))
+    cur.execute("SELECT * FROM events WHERE eventname = ?", name)
+    if cur.fetchone() != None:
+        await client.send(ctx.channel, "You have already created an event with this name")
+        return;
     
     date_info = (guild_id, sender, name, month, day, year)
 
@@ -42,13 +51,16 @@ async def schedule(ctx, name: str, month: int, day: int, year: int):
     date = f"{month}/{day}/{year}"    
     await ctx.send(f"Event '{name}' created for {date}")
 
-@bot.command()
-async def countdown(ctx, name: str):
+@bot.tree.command(name="countdown", description="get a countdown to your event")
+async def countdown(ctx, name: str) -> None:
     await ctx.send(f"Countdown for event '{name}' is not implemented yet")
 
-async def delete(ctx, name: str):
-    
+@bot.tree.command(name="delete", description="delete an event")
+async def delete(ctx, name: str) -> None:
+    sender = ctx.author
+    guild_id = ctx.guild.id
+    cur.execute("DELETE * FROM events WHERE guild = ? AND sender = ? AND eventname = ?", (guild_id, sender, name))
     await ctx.send("deleted")
 
 load_dotenv()
-bot.run(os.getenv("DISCORD_TOKEN"))
+bot.run(os.environ["DISCORD_TOKEN"])
